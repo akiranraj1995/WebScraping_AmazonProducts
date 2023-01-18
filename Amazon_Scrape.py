b0a19fc279c476e25c4a9488617e8ef342ca7953
A Kiran Raj <ambal.kiranraj@gmail.com>
2023-01-18 10:57:32 +0530

    Update Amazon_Scrape.py

--- Amazon_Scrape.py
+++ Amazon_Scrape.py
@@ -4,27 +4,29 @@ Created on Wed Jan  4 14:48:35 2023
 
 @author: User
 """
-
+#IMPORT THE REQUIRED LIBRARIES
 from selenium import webdriver
 from time import sleep
 #from selenium.webdriver.common.by import By
 
-#url='https://www.amazon.in/s?k=laptops&page='+str(int(1))
+#LINK TO LOAD THE PAGE
+url='https://www.amazon.in/s?k=laptops&page='+str(int(1))
+
+#LOADING THE WEBDRIVER
 driver=webdriver.Edge('C:/Users/User/Documents/EdgeWebdriver/msedgedriver.exe')
 driver.get('https://www.amazon.in')
 sleep(2)
 
-'''
-input_search=driver.find_element_by_xpath('.//input[@id="twotabsearchtextbox"]')
-search_button=driver.find_element_by_xpath('.//input[@id="nav-search-submit-button"]')
-'''
+#FINDING THE SEARCH BOX AND SUBMIT BOX
 input_search=driver.find_element_by_id("twotabsearchtextbox")
 search_button=driver.find_element_by_xpath('.//input[@type="submit"]')
 
+#ENTERING THE ELEMENT TO SEARCH
 input_search.send_keys("Laptops")
 sleep(2)
 search_button.click()
 
+#RANGE BASED ON THE NUMBER OF PAGES TO SCRAPE
 Products=[]
 for i in range(10):
     print("Scrapped pages:",i+1)
@@ -34,27 +36,10 @@ for i in range(10):
         sleep(2)
     next_page=driver.find_element_by_xpath('.//span[@class="s-pagination-item s-pagination-disabled"]')
     next_page.click()
-    
+  
+#LENGTH OF PRODUCTS
 len(Products)
 
 Products[5]
 
 Products
-''' 
-driver.find_elements_by_xpath('.//div[@class="sg-col-inner"]')
-el=driver.find_elements_by_xpath('.//div[@class="a-section"]')
-sleep(4)
-
-
-driver.find_elements_by_xpath('.//div[@class="sg-col-inner"]')
-el=driver.find_elements_by_tag_name('h2')
-for ele in el:
-    print(len(ele))
-
-links=driver.find_elements(By.TAG_NAME,"a")
-len(links)
-
-for link in links:
-    print(link.text)
-    
-'''
